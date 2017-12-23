45d9a3d9d3cc85c4afea635297c59f2eeb834d45
Aleksandra Obradovic <obradovic.n.aleksandra@gmail.com>
2017-12-23 04:22:13 +0100

    Minor fixes
    Fixing date representation on button

--- app/src/main/java/com/example/android/endate/ProductDetailFragment.java
+++ app/src/main/java/com/example/android/endate/ProductDetailFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -99,6 +100,7 @@ public class ProductDetailFragment extends Fragment {
     }
 
     private void updateDate() {
-        mDateButton.setText(mProduct.getExpirationDate().toString());
+        String formatDate = DateFormat.format("EEEE, MMM dd, yyyy", mProduct.getExpirationDate()).toString();
+        mDateButton.setText(formatDate);
     }
 }
